6c1c1c189a038ad77700720d976c8aa8f4e128b3
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-02-23 15:00:37 +0300

    Initial commit

---
+++ Crypto-T/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto-T
+//
+//  Created by Михаил Ковалевский on 23.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto-T/Crypto_TApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TApp.swift
+//  Crypto-T
+//
+//  Created by Михаил Ковалевский on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
